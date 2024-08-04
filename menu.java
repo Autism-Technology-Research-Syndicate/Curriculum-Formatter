469dd4d1d03ba5f13dc53c33c0882501a4480beb
Edward Chan <edwardchan2@comcast.net>
2024-08-03 18:10:34 -0700

    Created menu and export files

---
+++ menu.java
@@ -0,0 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import java.awt.Component;
+
+public class menu {
+
+    public static void main (String arg[]) {
+        JFrame frame = new JFrame("SEAL App GUI");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        frame.setSize(800, 800);
+        frame.setLayout(null);
+
+        
+
+        String choices[] = { "Talking","Eye Contact", "Emotion","Typing","Multiple choice","Slider",
+        "Video lessons","Reading","Teacher instruction","Pictures/diagrams","Animations of tasks",
+        "Conversational AI","Eye contact maintaining virtual avatar","In-person partner finding instruction paired with activity"};
+        
+    }
+    
+}
